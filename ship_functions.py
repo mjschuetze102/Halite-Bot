f877198e5ba4abcfae9ba90588bc51522542de93
mjschuetze102 <mjs8474@rit.edu>
2018-01-07 16:56:32 -0500

    Revert ship_functions back so that older versions of the bot will run

--- ship_functions.py
+++ ship_functions.py
@@ -22,20 +22,28 @@ def separate_ships(team_ships, command_queue):
         ship.change_role_settle()
         logging.info("Updated role: " + str(ship.role))
 
-def move(ship, target_planet, command_queue, game_map):
+def move(ship, target_planet, command_queue, planned_planets, game_map):
     # If the ship can dock at the planet do so
     # Else navigate to the planet
     if ship.can_dock(target_planet):
         command_queue.append(ship.dock(target_planet))
     else:
-        navigate_command = ship.navigate(
-            ship.closest_point_to(target_planet),
-            game_map,
-            speed=int(hlt.constants.MAX_SPEED),
-            angular_step=5,
-            ignore_ships=False
-        )
+        # If the planet is already being navigated to ignore it
+        # Else navigate to it
+        if target_planet in planned_planets:
+            return command_queue, planned_planets, 1
+        else:
+            navigate_command = ship.navigate(
+                ship.closest_point_to(target_planet),
+                game_map,
+                speed=int(hlt.constants.MAX_SPEED),
+                angular_step=5,
+                ignore_ships=False
+            )
 
-        # If we can successfully navigate to the planet, do so
-        if navigate_command:
-            command_queue.append(navigate_command)
+            # If we can successfully navigate to the planet, do so
+            if navigate_command:
+                command_queue.append(navigate_command)
+                planned_planets.append(target_planet)
+
+    return command_queue, planned_planets, 0
